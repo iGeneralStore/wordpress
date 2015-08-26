d4432c631e490043192d42f58e9b4f013858f928
root <root@ip-172-31-24-1.ap-southeast-1.compute.internal>
2015-08-26 02:29:47 +0000

    theme&custom-post-template

--- wp-content/themes/professional/sidebar-footer.php
+++ wp-content/themes/professional/sidebar-footer.php
@@ -4,24 +4,28 @@
  * @package professional
  */
  ?>
+<?
+// edit by KH
+// i update the parts which have col-sm-12
+?>
  <?php if ( is_active_sidebar('footer-1') || is_active_sidebar('footer-2') || is_active_sidebar('footer-3') ) : ?>
 	 <div id="footer-sidebar" class="widget-area">
 	 	<div class="container">
 		 	<?php 
 				if ( is_active_sidebar( 'footer-1' ) ) : ?>
-					<div class="footer-column col-md-4 col-sm-4"> 
+					<div class="footer-column col-sm-12"> 
 						<?php dynamic_sidebar( 'footer-1'); ?> 
 					</div> 
 				<?php endif;
 					
 				if ( is_active_sidebar( 'footer-2' ) ) : ?>
-					<div class="footer-column col-md-4 col-sm-4"> 
+					<div class="footer-column col-sm-12"> 
 						<?php dynamic_sidebar( 'footer-2'); ?> 
 					</div> 
 				<?php endif;
 		
 				if ( is_active_sidebar( 'footer-3' ) ) : ?>
-					<div class="footer-column col-md-4 col-sm-4"> <?php
+					<div class="footer-column col-sm-12"> <?php
 						dynamic_sidebar( 'footer-3'); ?> 
 					</div>
 				<?php endif; ?>
