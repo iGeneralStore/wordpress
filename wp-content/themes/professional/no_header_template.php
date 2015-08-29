2db80c2c060d8be1c92cb14447f30996650b265e
root <root@ip-172-31-24-1.ap-southeast-1.compute.internal>
2015-08-29 19:01:49 +0000

    setting is added, handling no_header

--- wp-content/themes/professional/no_header_template.php
+++ wp-content/themes/professional/no_header_template.php
@@ -1,37 +1,14 @@
 <?php
 /*
-Template Name: Headerless
+    Template Name: Headerless
 */
 ?>
-<style>
-/*
-@import url(http://fonts.googleapis.com/css?family=Noto+Sans:400,300,700);
-*/
-</style>
-
-<!--
-<link href="/assets/css/klassipie_notice.css" rel="stylesheet" media="all"/>
--->
-
-<!DOCTYPE html>
-
-<body>
-<meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<div style="position:absolute; background-color:efefef; z-index:0; margin:-8">
-<img src="http://localhost/wordpress/wp-content/uploads/2015/03/light-bulb_background.png" style="position:absolute; z-index:-5; height:60%; margin: -8">
-<br><br><br>
-<div id="klassipie-notice-content" style="width: 70%; margin-left:auto; margin-right:auto; font-family: 'Noto Sans', sans-serif;">
-	<div id="klassipie-notice-content-caption" style="color: F65A5E;text-align: center"><h2><a href="http://www.naver.com">Klassi-Pie가 새로워졌습니다</a></h2></div>
-<div style="height:1px;font-size:1px;">&nbsp;</div>
-	<div id="klassipie-notice-content-primary">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<?php 
-			//get_template_part( 'content', 'page' ); 
-			the_content();
-			?>
-			<?php endwhile; // end of the loop. ?>
-	</div>
+
+<div style="width:100%; height:100%">
+<?php while ( have_posts() ) : the_post(); ?>
+<?php 
+    the_content();
+?>
+<?php endwhile; // end of the loop. ?>
 </div>
-</div>
-</body>
