7a02b1bd1313978717c51a1f9e6e3e2143c94c4f
root <root@ip-172-31-24-1.ap-southeast-1.compute.internal>
2015-09-29 14:23:42 +0000

    favicon

--- wp-content/themes/professional/header.php
+++ wp-content/themes/professional/header.php
@@ -14,6 +14,8 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
 </head>
 <body <?php body_class(); ?>>
 <?php global $option_setting; ?>
